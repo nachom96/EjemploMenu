0b22bfc2db8c4282206905146c9e86a550636dc0
Nacho <nacho_085@hotmail.com>
2022-03-05 16:14:40 +0100

    Añadido Menu y Toast al darle a Settings

--- app/src/main/java/com/example/ejemplomenu/MainActivity.kt
+++ app/src/main/java/com/example/ejemplomenu/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.ejemplomenu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_activity, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.mnuSettings -> navigateToSettings()
+            else -> return super.onOptionsItemSelected(item)
+        }
+        return true
+    }
+
+    private fun navigateToSettings() {
+        Toast.makeText(this, "Navegar a Settings", Toast.LENGTH_SHORT).show()
     }
 }
